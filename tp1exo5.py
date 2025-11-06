35968e1e93a21127cf3c0cb167e84d5409ad2d2d
Alessandro-Bauer-Sicari <alessandro68260@gmail.com>
2025-11-06 16:28:45 +0100

    Delete tp1exo5.py

--- tp1exo5.py
+++
@@ -1,9 +0,0 @@
-jour = int(input("Entrez le numéro du jour (ex : 3) : "))
-heure = int(input("Entrez l'heure actuelle (0 à 23) : "))
-minute = int(input("Entrez les minutes actuelles (0 à 59) : "))
-
-
-minutes_ecoulees = ((jour - 1) * 24 * 60) + (heure * 60) + minute
-
-
-print(f"\nDepuis le début du mois, il s'est écoulé {minutes_ecoulees} minutes.")
